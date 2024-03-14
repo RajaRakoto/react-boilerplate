5de2d5420be9bc714bdb6288e681844d5e859522
raja <raja.rakoto7@gmail.com>
2024-03-14 17:57:03 +0300

    scripts: add sass-charset

---
+++ scripts/sass-charset.js
@@ -0,0 +1,50 @@
+/**
+ * This script adds the `@charset "UTF-8";` declaration at the beginning of all SCSS files in the project.
+ */
+
+const fs = require("fs");
+const path = require("path");
+
+// recursively browse files in a directory
+function traverseDirectory(dir) {
+	fs.readdirSync(dir).forEach((file) => {
+		const filePath = path.join(dir, file);
+		const stat = fs.statSync(filePath);
+
+		if (stat.isDirectory()) {
+			// ignore node_modules
+			if (file !== "node_modules") {
+				traverseDirectory(filePath);
+			}
+		} else {
+			if (path.extname(file) === ".scss") {
+				processSCSSFile(filePath);
+			}
+		}
+	});
+}
+
+// add utf8 at the beginning of an SCSS file
+function processSCSSFile(filePath) {
+	const data = fs.readFileSync(filePath, "utf8");
+	const hasDoubleQuotes = data.startsWith('@charset "UTF-8";');
+	const hasSingleQuotes = data.startsWith("@charset 'UTF-8';");
+
+	if (!hasDoubleQuotes && !hasSingleQuotes) {
+		const newData = `@charset "UTF-8";\n\n${data}`;
+		fs.writeFileSync(filePath, newData);
+		console.log(`${path.relative(process.cwd(), filePath)} encoded ... [done]`);
+	} else {
+		console.log(
+			`${path.relative(process.cwd(), filePath)} already encoded, skipping ...`,
+		);
+	}
+}
+
+// entry point
+function startCharset() {
+	const projectRoot = path.join(__dirname, "..");
+	traverseDirectory(projectRoot);
+}
+
+startCharset();
