80039c32875a618676545ed33cedd3d1925b44ff
raja <raja.rakoto7@gmail.com>
2024-03-14 18:33:30 +0300

    src: add pages struct

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+/* types */
+import { T_FunctionComponent } from "../types";
+
+// =======================================
+
+export default function Home(): T_FunctionComponent {
+	return (
+		<div className="bg-blue-300  font-bold w-screen h-screen flex flex-col justify-center items-center ">
+			<p className="text-white text-6xl">Hello, world !</p>
+		</div>
+	);
+}
