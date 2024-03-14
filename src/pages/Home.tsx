5e20d69a232947910daddeb1d54a8fee7791ee38
raja <raja.rakoto7@gmail.com>
2024-03-14 20:33:53 +0300

    src: hotfix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import { T_FunctionComponent } from "../types";
 
 export default function Home(): T_FunctionComponent {
 	return (
-		<div className="bg-blue-300  font-bold w-screen h-screen flex flex-col justify-center items-center ">
+		<div className="bg-blue-300 font-bold w-screen h-screen flex flex-col justify-center items-center">
 			<p className="text-white text-6xl">Hello, world !</p>
 		</div>
 	);
