1ce70858f1f4e14c421a8e6b418fa53ee1e05646
raja <raja.rakoto7@gmail.com>
2024-03-15 10:55:22 +0300

    src: add T_Mail + I_User

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,10 @@
 export type T_FunctionComponent = React.ReactElement | null;
+
+export type T_Mail = `${string}@${string}.com`;
+
+export interface I_User {
+	id: string;
+	email: T_Mail;
+	name: string;
+	admin: boolean;
+}
