dbbf9d43d82f5f4f49a22b5360a004ddb29a4f69
raja <raja.rakoto7@gmail.com>
2024-03-24 17:37:22 +0300

    add resolve path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": "/src",
+			"~": "/public",
+		},
+	},
 	server: {
 		host: true,
 		strictPort: true,
