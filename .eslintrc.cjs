d4b61d4956512af209995b69130fa7da31706acc
raja <raja.rakoto7@gmail.com>
2024-09-10 19:35:25 +0300

    eslint: valid config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,45 @@
 module.exports = {
+  root: true,
 	env: {
 		browser: true,
-		es6: true
+		es6: true,
+		node: true,
 	},
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: './tsconfig.json',
-		sourceType: 'module',
-		ecmaVersion: 'latest'
-	},
-	plugins: ['@typescript-eslint', 'jest', 'react-refresh'],
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
-		'plugin:jest/recommended',
 		'plugin:react-hooks/recommended',
+		'plugin:jest/recommended',
 		'prettier'
 	],
-	overrides: [
-		{
-			files: ['*.{js,cjs,mjs,ts,mts,jsx,tsx}']
-		}
-	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: ["./tsconfig.json"],
+		ecmaVersion: 'latest',
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	plugins: ['@typescript-eslint', 'jest', 'react-refresh'],
 	rules: {
-		'no-unused-vars': 'warn',
-		'@typescript-eslint/explicit-function-return-type': 'warn',
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		"@typescript-eslint/no-inferrable-types": "off",
+		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/no-empty-interface": "off",
+		"@typescript-eslint/no-namespace": "off",
+		"@typescript-eslint/no-empty-function": "off",
+		"@typescript-eslint/no-this-alias": "off",
+		"@typescript-eslint/ban-types": "off",
+		"@typescript-eslint/ban-ts-comment": "off",
+		"prefer-spread": "off",
+		"no-case-declarations": "off",
+		"no-console": "off",
+		"@typescript-eslint/no-unused-vars": ["warn"],
+		"@typescript-eslint/consistent-type-imports": "warn",
+		"@typescript-eslint/no-unnecessary-condition": "warn",
 		'react-refresh/only-export-components': [
 			'warn',
 			{ allowConstantExport: true }
